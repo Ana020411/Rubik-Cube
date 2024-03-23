b335358640e6e35f245e4ab61d8b1a75fe4b57ee
carolinavef <163360189+carovelasco@users.noreply.github.com>
2024-03-22 18:00:00 -0600

    movimientos en x

--- Proyecto_intermedio_Rubik_Cube.py
+++ Proyecto_intermedio_Rubik_Cube.py
@@ -4,66 +4,76 @@ class RubikCube:
     def __init__(self):
         # Lista de listas para representar cada cara del cubo
         self.cube = [
-            [[0 for _ in range(3)] for _ in range(3)],  # Cara superior
-            [[1 for _ in range(3)] for _ in range(3)],  # Cara inferior
-            [[2 for _ in range(3)] for _ in range(3)],  # Cara izquierda
-            [[3 for _ in range(3)] for _ in range(3)],  # Cara derecha
-            [[4 for _ in range(3)] for _ in range(3)],  # Cara frontal
+            [[0 for _ in range(3)] for _ in range(3)],  # Cara Frente
+            [[1 for _ in range(3)] for _ in range(3)],  # Cara derecha
+            [[2 for _ in range(3)] for _ in range(3)],  # Cara superior
+            [[3 for _ in range(3)] for _ in range(3)],  # Cara inferior
+            [[4 for _ in range(3)] for _ in range(3)],  # Cara izquierda
             [[5 for _ in range(3)] for _ in range(3)]   # Cara trasera
+            
         ]
+        
+        print(len(self.cube))
+        print(len(self.cube[0]))
+        print(len(self.cube[0][0]))
+        #6 en y, 3 en x y z       
 
     def print_cube(self):
         for cara in self.cube:
             for fila in cara:
                 print(fila)
             print()
-    
-    def __xmove(self, n_row, n_moves):
-        self.cube[n_row] = [self.cube[n_row][(i - n_moves) % 3] for i in range(3)]
-
-    def __ymove(self, n_col, n_moves):
-        for i in range(3):
-            self.cube[i][n_col] = self.cube[(i - n_moves) % 3][n_col]
-
-    def __zmove(self, n_depth, n_moves):
-        for i in range(3):
-            self.cube[i] = [self.cube[i][j] for j in range(n_moves, 3)] + [self.cube[i][j] for j in range(n_moves)]
-
+            
+            #BOTTOM DERECHA Y BACK
     # Gira la fila superior en sentido horario
-    def move_xup(self, n_moves):
-        self.__xmove(0, n_moves)
+    def move_xup(self, n_moves):#AP
+        for _ in range(n_moves):
+            aux0 = self.cube[0][0].copy()  
+            aux1 = self.cube[1][0].copy()  
+            aux5 = self.cube[5][0].copy()  
+            aux4 = self.cube[4][0].copy()  
 
-    # Gira la fila central en sentido horario
-    def move_xmiddle(self, n_moves):
-        self.__xmove(1, n_moves)
+            self.cube[1][0] = aux0  
+            self.cube[5][0] = aux1  
+            self.cube[4][0] = aux5  
+            self.cube[0][0] = aux4  
+            
+    def move_xmiddle(self, n_moves):#C
+        self.move_xbottom(n_moves)
+        self.move_xup(n_moves)
+        
+    def move_xbottom(self, n_moves):#AP
+        for _ in range(n_moves):
+            aux0 = self.cube[0][2].copy()  
+            aux1 = self.cube[1][2].copy()  
+            aux5 = self.cube[5][2].copy()  
+            aux4 = self.cube[4][2].copy()  
 
-    # Gira la fila inferior en sentido horario
-    def move_xbottom(self, n_moves):
-        self.__xmove(2, n_moves)
+            self.cube[1][2] = aux0  
+            self.cube[5][2] = aux1  
+            self.cube[4][2] = aux5  
+            self.cube[0][2] = aux4  
 
+                
     # Gira la columna izquierda en sentido horario
     def move_yleft(self, n_moves):
-        self.__ymove(0, n_moves)
+        pass
 
     # Gira la columna central en sentido horario
-    def move_ymiddle(self, n_moves):
-        self.__ymove(1, n_moves)
-
+    def move_ymiddle(self, n_moves):#AP
+        pass
     # Gira la columna derecha en sentido horario
     def move_yright(self, n_moves):
-        self.__ymove(2, n_moves)
-
+        pass
     # Gira la cara frontal en sentido horario
-    def move_zfront(self, n_moves):
-        self.__zmove(0, n_moves)
-
+    def move_zfront(self, n_moves):#AP
+        pass
     # Gira la cara del medio en sentido horario
     def move_zmiddle(self, n_moves):
-        self.__zmove(1, n_moves)
-
+        pass
     # Gira la cara trasera en sentido horario
-    def move_zback(self, n_moves):
-        self.__zmove(2, n_moves)
+    def move_zback(self, n_moves):#YO
+        pass        #ACCEDER A LA 0 DE LA 5 
 
     def __move_cube(self, move):
         if move == 0:
@@ -71,7 +81,7 @@ class RubikCube:
         elif move == 1:
             self.move_xbottom(1)
         elif move == 2:
-            self.move_xmiddle(1)
+            self.move_xmiddle(3)
         elif move == 3:
             self.move_yleft(1)
         elif move == 4:
@@ -84,22 +94,43 @@ class RubikCube:
             self.move_zfront(1)
         elif move == 8:
             self.move_zmiddle(1)
+        elif move == 9:
+            self.move_xup(3)
+        elif move == 10:
+            self.move_xbottom(3)
+        elif move == 11:
+            self.move_xmiddle(1)
+        elif move == 12:
+            self.move_yleft(3)
+        elif move == 13:
+            self.move_ymiddle(3)
+        elif move == 14:
+            self.move_yright(3)
+        elif move == 15:
+            self.move_zback(3)
+        elif move == 16:
+            self.move_zfront(3)
+        elif move == 17:
+            self.move_zmiddle(3)
 
-    def __count_colors(self):
-        pass
-
-    def shuffle(self, n):
-        movimientos_posibles = [self.move_xup, self.move_xmiddle, self.move_xbottom, self.move_yleft, self.move_ymiddle, self.move_yright, self.move_zfront, self.move_zmiddle, self.move_zback]
-
-        for _ in range(n):
-            movimiento_aleatorio = random.choice(movimientos_posibles)
-            movimiento_aleatorio(1)
+    def shuffle(self, n_moves, make_moves=[]):
+        if len(make_moves) > 0:
+            for move in make_moves:
+                self.__move_cube(move)
+                self.print_cube()
+        else: 
+            for i in range(n_moves):
+                move = random.randint(0, 17) #varia el 11
+                print(move)
+                self.__move_cube(move)
 
     def solve(self):
         pass
 
 cubo = RubikCube()
 cubo.print_cube()
-cubo.shuffle(10)
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+print("-----------------------------------------------")
+
+cubo.move_xbottom(1)
+cubo.move_xup(1)
 cubo.print_cube()
